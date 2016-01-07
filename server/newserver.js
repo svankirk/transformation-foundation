89eaab6e0b8280977cd3a1e7a3f0c4a203427270
labratech <legshatter@gmail.com>
2016-01-07 06:29:52 +0000

    removed server

--- server/newserver.js
+++
@@ -1,9 +0,0 @@
-var express=require("express");
-var app=express();
-app.post("/jsform", function(req,res) {
-   console.log(req);
-   res.send("response",200);
-});
-app.listen(process.env.PORT, function(){
-    console.log("server is running on port " + process.env.PORT);
-});
--- server/settings.js
+++
@@ -1,8 +0,0 @@
-exports.listenPort = "9000";
-exports.getUrl = "/application";
-exports.database = "test";
-exports.collection = "applications";
-exports.dbURL = "mongodb://localhost:27017";
-exports.UNKNOWN = 404;
-exports.SUCCESS = 200;
-exports.FAILURE = 500;
